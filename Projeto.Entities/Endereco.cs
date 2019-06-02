619ec1e398d156bce6cd379cc918f5277ec50370
rogeriobsoliveira2006 <rogeriobsoliveira2006@outlook.com>
2019-06-02 14:09:48 -0300

    Subindo Projeto para o GitHub

---
+++ Projeto.Entities/Endereco.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entities
+{
+    public class Endereco
+    {
+        //propriedades...
+        public int EnderecoId { get; set; }
+        public string Logradouro { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public string Cep { get; private set; }
+
+        //Associação TER-1
+        public Pessoa Pessoa { get; set; }
+
+        //contrutor...
+        public Endereco()
+        {
+            //vazio....
+        }
+
+        //sobrecarga de contrutores(overloading)
+        public Endereco(int enderecoId, string logradouro, string cidade, string estado, string cep)
+        {
+            EnderecoId = enderecoId;
+            Logradouro = logradouro;
+            Cidade = cidade;
+            Estado = estado;
+            Cep = cep;
+        }
+
+        //Encapsulamento Logradouro da Pessoa
+        public void AddLogradouro(string logradouro)
+        {
+            Logradouro = logradouro;
+        }
+
+        //Encapsulamento Cidade da Pessoa
+        public void AddCidade(string cidade)
+        {
+            Cidade = cidade;
+        }
+
+        //Encapsulamento Estado da Pessoa
+        public void AddEstado(string estado)
+        {
+            Estado = estado;
+        }
+
+        //Encapsulamento CEP da Pessoa
+        public void AddCEP(string cep)
+        {
+            Cep = cep;
+        }
+
+        //sobrescrita de método ToString()
+        public override string ToString()
+        {
+            return $"ID: {EnderecoId} | Logradouro: {Logradouro} | Cidade: {Cidade} | Estado: {Estado} | CEP: {Cep} ";
+        }
+    }
+}
---
+++ Projeto.Entities/Pessoa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entities
+{
+    public class Pessoa
+    {
+        //propriedades...
+        public int PessoaId { get; set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+
+        //Associação TER-1
+        public Endereco Endereco { get; set; }
+
+        //contrutor...
+        public Pessoa()
+        {
+            //Vazio...
+        }
+
+        //sobrecarga de contrutores(overloading)
+        public Pessoa(int pessoaId, string nome, string email, DateTime dataCadastro)
+        {
+            PessoaId = pessoaId;
+            Nome = nome;
+            Email = email;
+            DataCadastro = dataCadastro;
+        }
+
+        //Encapsulamento Nome Pessoa
+        public void AddNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        //Encapsulamento Email Pessoa
+        public void AddEmail(string email)
+        {
+            Email = email;
+        }
+
+        //Encapsulamento Data Cadastro Pessoa
+        public void AddDataCadastro(DateTime dataCadastro)
+        {
+            DataCadastro = dataCadastro;
+        }
+
+        //sobrescrita de método ToString()..
+        public override string ToString()
+        {
+            return $"ID: {PessoaId} | Nome: {Nome} | Email: {Email} | Data de Cadastro: {DataCadastro} .";
+        }
+    }
+}
